8234f1789ddaea28d0be42ea13b19805b1334962
hackovic <hackovic@e6bc83f8-c48b-11de-8b01-0f61d55110ad>
2009-10-29 13:09:42 +0000

    Create project

---
+++ ColorVisualizer/ColorForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ColorVisualizer
+{
+	public partial class ColorForm : Form
+	{
+		public ColorForm()
+		{
+			InitializeComponent();			
+			panelMain.BackColor = SystemColors.Control;
+			labelRGB.Text = "";
+			labelNamedColor.Text = "";
+		}
+
+		private Color color;
+		public Color Color {
+			get { return color; }
+			set 
+			{ 				
+				color = value;
+				labelRGB.Text = string.Format("A={0},  R={1}, G={2}, B={3}", color.A, color.R, color.G , color.B);
+				labelNamedColor.Text = color.ToKnownColor().ToString();
+				labelName.Text = color.Name;
+				panelMain.BackColor = color;
+				buttonClose.ForeColor = color;
+				buttonClose.BackColor = SystemColors.Control;				
+			} 
+		}
+
+		private void ColorForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyData == Keys.Escape 
+				|| e.KeyData == Keys.Return 
+				|| e.KeyData == Keys.Enter) {
+				this.Close();
+			}
+		}
+
+		private void buttonClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
