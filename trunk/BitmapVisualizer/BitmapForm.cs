33e6506388c44a70ace56c6d688440c3882caf48
hackovic <hackovic@e6bc83f8-c48b-11de-8b01-0f61d55110ad>
2010-02-12 08:15:17 +0000

    set form to size of image. Add size info in title

--- trunk/BitmapVisualizer/BitmapForm.cs
+++ trunk/BitmapVisualizer/BitmapForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
 using System.Drawing;
+using System.Windows.Forms;
 
 
 namespace BitmapVisualizer
@@ -17,10 +15,38 @@ namespace BitmapVisualizer
         public Bitmap Bitmap
         {
             get { return aBitmap; }
-            set { aBitmap = value;
-            this.pictureBox.Image = value;
+            set
+            {
+                aBitmap = value;
+                this.pictureBox.Image = value;
+                SetTitle();
+                ResizeFormToFitImage();
             }
         }
+
+        private void SetTitle()
+        { 
+            if (this.pictureBox.Image == null)
+                return;
+
+            Text += string.Format(" - {0} x {1} pixel", this.pictureBox.Image.Size.Width, this.pictureBox.Image.Size.Height);
+        }
+
+        private void ResizeFormToFitImage()
+        {
+            if (this.pictureBox.Image == null)
+                return;
+
+            int width = this.pictureBox.Image.Size.Width + (this.Width - this.ClientRectangle.Width);
+            int height = this.pictureBox.Image.Size.Height + (this.Height  - this.ClientRectangle.Height) + this.panelBottom .Height ;
+            
+            // Smaller then current screen
+            width = Math.Min(width, Screen.FromControl(this).WorkingArea.Width - 100);
+            height  = Math.Min(height , Screen.FromControl(this).WorkingArea.Height- 100);
+            this.Size = new Size(width, height);
+            CenterToScreen();
+        }
+
         private void buttonClose_Click(object sender, EventArgs e)
         {
             this.Close();
