d9b54be85ab19e9b4a9a725577ab1b485b2525e5
hackovic <hackovic@e6bc83f8-c48b-11de-8b01-0f61d55110ad>
2011-11-27 17:42:15 +0000

    New DataRow visualizer, Context menu on datagridview added with filter on prelation and selected cells

--- trunk/TestColorVisualizer/TestVSVisualizer.cs
+++ trunk/TestColorVisualizer/TestVSVisualizer.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Windows.Forms;
 using Hackovic.VisualStudio.NumberVisualizer;
 
 namespace Hackovic.VisualStudio
@@ -12,10 +13,10 @@ namespace Hackovic.VisualStudio
     [System.STAThreadAttribute]
     static void Main()
     {
-      TestNumber();
+      //TestNumber();
       //TestColorVis();
       //TestBitmapVisualizer();
-      //TestDataSetVisaulizer();
+      TestDataSetVisaulizer();
     }
 
     private static void TestNumber()
@@ -29,25 +30,73 @@ namespace Hackovic.VisualStudio
       DataTable t1 = ds.Tables.Add( "T1" );
       DataTable t2 = ds.Tables.Add( "T2" );
       DataTable t3 = ds.Tables.Add( "T3" );
+      DataTable t4 = ds.Tables.Add( "T4" );
 
-      t1.Columns.Add( "T11" );
-      t1.Columns.Add( "T12" );
+    var t1_c1 =  t1.Columns.Add( "T11" );
+    t1.Columns.Add( "T12" );
       t1.Columns.Add( "T13" );
-      var row = t1.NewRow();
-      row[0] = "1";
-      row[1] = "2";
-      row[2] = "3";
-      t1.Rows.Add( row );
-      t1.AcceptChanges();
 
+      
 
-      t2.Columns.Add( "T21" );
-      t2.Columns.Add( "T22" );
+    var t2_c1 =    t2.Columns.Add( "T21" );
+    t2.Columns.Add( "T22" );
       t2.Columns.Add( "T23" );
 
-      t3.Columns.Add( "T31" );
+      var t3_c1 = t3.Columns.Add( "T31" );
+      t4.Columns.Add( "T41" );
+    
+      t1.ParentRelations.Add( t2_c1, t1_c1 );
+      t1.ChildRelations.Add( t1_c1, t3_c1 );
 
+      var r = t2.NewRow();
+      r[0] = "10";
+      r[1] = "2.0";
+      r[2] = "2.0";
+      t2.Rows.Add( r );
+      r = t2.NewRow();
+      r[0] = "11";
+      r[1] = "2.0";
+      r[2] = "2.3";
+      t2.Rows.Add( r );
+
+      var row = t1.NewRow();
+      row[0] = "10";
+      row[1] = "20";
+      row[2] = "30";
+      t1.Rows.Add( row );
+
+      row = t1.NewRow();
+      row[0] = "11";
+      row[1] = "21";
+      row[2] = "31";
+      t1.Rows.Add( row );
+
+
+     
+       var r2 = t3.NewRow();
+      r2[0] = "10";
+      t3.Rows.Add( r2 );
+
+      var r41 = t4.NewRow();
+      r41[0] = DateTime.Today;
+      t4.Rows.Add(r41);
+      var r42 = t4.NewRow();
+      r42[0] = DateTime.Now;
+      t4.Rows.Add( r42 );
+
+      ds.AcceptChanges();
+      BindingSource bindingSource = new BindingSource( ds,ds.Tables[0].TableName );
+      
+      DataGridView dataGridView = new DataGridView ();
+      //dataGridView.DataSource = bindingSource;
+      dataGridView.Columns.Add("C1", "C1");
+      dataGridView.Rows .Add( new [] { "11/22/1968"})
+      ;
+      Console.WriteLine( dataGridView.Rows.Count);
       DataSetVisualizer.TestShowVisualizer( ds );
+      //DataTableVisualizer.TestShowVisualizer(t1);
+      //DataRowVisualizer.TestShowVisualizer(row);
+      //DataGridViewRowVisualizer.TestShowVisualizer( dataGridView.Rows[0] );
     }
 
     private static void TestColorVis()
